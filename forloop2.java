62063effc96a4499d2994631b2232ae7007653cb
shubhammane8668 <shubhsmane@gmail.com>
2022-07-04 19:26:43 +0530

    reverse of numbers

---
+++ forloop2.java
@@ -0,0 +1,19 @@
+
+public class forloop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+int number = 123456, reverse = 0;  
+//we have not mentioned the initialization part of the for loop  
+for(;number != 0; number=number/10)   
+{  
+int remainder = number % 10;  
+reverse = reverse * 10 + remainder;  
+}  
+System.out.println("The reverse of the given number is: " + reverse);
+
+	}
+
+}
