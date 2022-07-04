f5500804a398078b8408e62b3384bc9f9460a3d5
shubhammane8668 <shubhsmane@gmail.com>
2022-07-04 19:29:32 +0530

    natural numbers

---
+++ switch1.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class switch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   char ch;//variable declaration
+		   Scanner scan=new Scanner(System.in); 
+		      //create a scanner object for input
+		      
+		  System.out.println("Enter the Alphabet for check vowel or consonant  ");
+		  ch=scan.next().charAt(0);;// store the input from the user
+		  switch(ch){
+		    //check lower case vowel letters
+	case 'a':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'e':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'i':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'o':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'u':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    //check upper case vowel letters
+		    case 'A':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'E':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'I':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'O':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'U':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    default:
+		    System.out.println(ch+" is a consonant");
+		    break;
+		    
+		    
+		}
+	}
+}
