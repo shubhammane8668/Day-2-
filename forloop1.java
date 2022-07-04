fa99561958ec7e836bd2248dc26c8b190acbc8a8
shubhammane8668 <shubhsmane@gmail.com>
2022-07-04 19:23:22 +0530

    sum of natural numbers

---
+++ forloop1.java
@@ -0,0 +1,15 @@
+
+public class forloop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int i=1,num=5,sum=0;
+   
+		   for (i=1;i<=num;i++);
+		   {
+			   sum=sum+i;
+		   }
+		   System.out.println("sum of numbers is: " +sum);
+	}
+
+}
